636f4a785c31d51de955e6e1bc3668dc48e01f1c
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-06 00:46:02 -0300

    But in the end, it doesn't even worked

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,7 +1,18 @@
-iframeContent.addEventListener("load", () => {
-  loadingScreen.style.display = "none";
-})
+function checkIframeLoad() {
+  const loadingScreen = document.querySelector(".loading");
+  const iframe = document.querySelector("iframe#room3d");
+  const iframeContent = iframe.contentDocument;
 
-const loadingScreen = document.querySelector(".loading");
-const iframe = document.querySelector("iframe#room3d");
-const iframeContent = iframe.contentDocument;
+  if (iframeContent.readyState == "complete") {
+    iframe.contentWindow.onload = () => {
+      alert("I'm loading");
+    }
+
+    loaded();
+    return;
+  }
+}
+
+function loaded() {
+  alert("It's working :D")
+}
