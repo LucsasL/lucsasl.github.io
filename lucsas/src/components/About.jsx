a274837980dbede4ac30a178620ab9cf8d815cc4
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-27 20:47:00 -0300

    Services + About

--- lucsas/src/components/About.jsx
+++ lucsas/src/components/About.jsx
@@ -1,9 +1,52 @@
 function About() {
   return (
     <>
-    
+      <div id="about-style"></div>
+      <section id="about">
+        <div class="cont">
+          <div class="hidden">
+            <h1 class="big">
+              Hi, My name is <span class="fancy">Lucas Lira</span>
+            </h1>
+            <p>
+              I work as a{" "}
+              <em style="font-weight: bold;">Pay-Per-Click Manager,</em> a
+              professional which main function is to create, manage and
+              optimizing ads campaign in different online advertising softwares,
+              but the most important function, is to bring results for my
+              clients.
+            </p>
+
+            <p>
+              I've been studying topics surrounding the fields of Digital
+              Marketing and Programming, always looking to improve and get
+              better in delivering what you need.
+            </p>
+
+            <p>
+              My job and commitment goes to give results, whether this being
+              conversions, engagement, leads or even more recognition to your
+              work.
+            </p>
+
+            <p>What do you thing? Can we grow together?</p>
+          </div>
+
+          <div class="img-div hidden1">
+            <figure>
+              <iframe
+                src="https://my.spline.design/untitled-1cb26d4fcf3ebd2bc106efd6a761ebbc/"
+                title="Lucsas Mascot"
+                width="100%"
+                height="100%"
+                id="mascot"
+              ></iframe>
+            </figure>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
 export default About;
--- lucsas/src/components/Services.jsx
+++ lucsas/src/components/Services.jsx
@@ -1,7 +1,76 @@
 function Services() {
   return (
     <>
-    
+      <section id="skills">
+        <div>
+          <div class="center">
+            <h1 class="block-text big hidden1" style={{ margin: "30px 0px" }}>
+              In what can I help you?
+            </h1>
+          </div>
+
+          <div div id="cards">
+            <div class="skill-block hidden">
+              <div class="img-sk">
+                <figure>
+                  <img src="img/startup.webp" alt="Connection with your target audience" class="functions" />
+                </figure>
+              </div>
+            </div>
+
+            <div class="img-sk"></div>
+
+            <div>
+              <h2 style={{ fontSize: "2em" }}>
+                Campaign Creation
+              </h2>
+              
+              <p>
+                Create optimized campaigns to potential clients in different segmentation and locations. Making your business reach new clients in a better way and promoting your branch.
+              </p>
+            </div>
+          </div>
+
+          <div class="skill-block hidden">
+            <div class="img-sk">
+              <figure>
+                <img src="img/promote.webp" alt="Promote your brand" class="functions" />
+              </figure>
+            </div>
+
+            <div class="img-sk"></div>
+
+            <div>
+              <h2 style={{ fontSize: "2em" }}>
+                Captivating Writing
+              </h2>
+              <p>
+                With good use of Copywriting, we can convince more people and attract impressions, leads and conversions.
+              </p>
+            </div>
+          </div>
+
+          <div class="skill-block hidden">
+            <div class="img-sk">
+              <figure>
+                <img src="img/wireframe.webp" alt="Multi platform adverts" class="functions" />
+              </figure>
+            </div>
+
+            <div class="img-sk"></div>
+
+            <div>
+              <h2 style={{ fontSize: "2em" }}>
+                Multi-Platform
+              </h2>
+          
+              <p>
+                Expanding your brand in different social medias and all different places of internet, bringing more sight for you.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
