ba00ee76cbc81dbc68aab4310915f6833bdd9998
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-27 20:51:59 -0300

    Update Portfolio.jsx

--- lucsas/src/components/Portfolio.jsx
+++ lucsas/src/components/Portfolio.jsx
@@ -1,9 +1,68 @@
 function Portfolio() {
   return (
     <>
-    
+      <section id="functions">
+        <div class="cont">
+          <div class="results">
+            <div class="center">
+              <h1 class="block-text big">What does a traffic manager do?</h1>
+            </div>
+
+            <div id="prom-container">
+              <div class="promises hidden1">
+                <p>
+                  Make you reach any type of person on the internet through ads
+                  campaign.
+                </p>
+              </div>
+
+              <div class="promises hidden1">
+                <p>
+                  Make your clients pay attention to your products or services
+                  on the biggest attention attractor, the internet.
+                </p>
+              </div>
+
+              <div class="promises hidden1">
+                <p>
+                  Often reports of the results of your campaigns and the
+                  strategies used to make it.
+                </p>
+              </div>
+
+              <div class="promises hidden1">
+                <p>
+                  Use strategies and segmentation to appear to the persons that
+                  matter the most to you.
+                </p>
+              </div>
+
+              <div class="promises hidden1">
+                <div>
+                  <p>
+                    Sell the right product, to the right person in the right
+                    time, focusing only in the target audience of yours.
+                  </p>
+                </div>
+
+                <div>
+                  <figure>
+                    <iframe
+                      src="https://my.spline.design/untitled-f9173bd99cf55847c431ed275730d732/"
+                      frameborder="0"
+                      width="100%"
+                      height="100%"
+                      title="Quality Traffic to your business"
+                    ></iframe>
+                  </figure>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
 export default Portfolio;
