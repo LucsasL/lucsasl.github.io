90b7c25baec55b9d8a624020b397b7b8bd9808ea
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-27 20:33:55 -0300

    Update Introsect.jsx

--- lucsas/src/components/Introsect.jsx
+++ lucsas/src/components/Introsect.jsx
@@ -1,7 +1,78 @@
 function Introsect() {
   return (
     <>
-    
+      <section id="intro">
+        <div class="intro3D">
+          <iframe src='https://my.spline.design/building-39f31bffe96091b3362e6606e3cbfd11/' title="" id="room3d" width='100%' height='100%'></iframe>
+        </div>
+
+        <div class="cont" style={{ display: "none" }}>
+          <div class="intro">
+            <div id="introducing">
+              <h1>
+                <span style={{ fontSize: ".9em", lineHeight: "1em" }} >Sup, I'm</span> <br />
+                <span class="nickname">Lucsas</span>
+                <span class="cursiveWords">Lucas Lira</span>
+              </h1>
+              <h2 class="fancy intro-h2">
+                PPC Manager
+              </h2>
+
+              <p>
+                I create and optimize adverts campaign in Google Ads and Meta Ads, delivering the best to engage and convert.
+              </p>
+            
+              <button id="skillsResume">
+                <a href="" download="">Download Resume</a>
+              </button>
+            </div>
+            <div id="socials">
+              <div>
+                <figure>
+                  <figcaption>
+                  </figcaption>
+                  <a href="https://www.instagram.com/lucsas.l/" target="_blank" rel="noopener noreferrer">
+                    <img src="img/instagram-social.webp" alt="Lucsas Instagram" class="icons" />
+                  </a>
+                </figure>
+              </div>
+              <div>
+                <figure>
+                  <figcaption>
+                  </figcaption>
+                  <a href="http://github.com/lucsasl" target="_blank" rel="noopener noreferrer">
+                    <img src="img/github-social.webp" alt="Lucsas GitHub" class="icons" />
+                  </a>
+                </figure>
+              </div>
+              <div>
+                <figure>
+                  <figcaption>
+                  </figcaption>
+                  <a href="https://www.linkedin.com/in/lucsas/?locale=en_US" target="_blank" rel="noopener noreferrer">
+                    <img src="img/linkedin-social.webp" alt="Lucsas Linkedin" class="icons" />
+                  </a>
+                </figure>
+              </div>
+              <div>
+                <figure>
+                  <figcaption>
+                  </figcaption>
+                  <a href="https://twitter.com/LucsasL" target="_blank" rel="noopener noreferrer">
+                    <img src="img/twitter-social.webp" alt="Lucsas Twitter" class="icons" />
+                  </a>
+                </figure>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="scroll-anim">
+          <div>
+
+          </div>
+        </div>
+      </section>
     </>
   )
 }
