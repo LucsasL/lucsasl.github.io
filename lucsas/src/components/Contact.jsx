f5237d3431cadce0205b866e40aa28b790507e96
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-28 17:58:14 -0300

    Contact Style Ready

--- lucsas/src/components/Contact.jsx
+++ lucsas/src/components/Contact.jsx
@@ -26,28 +26,25 @@ function Contact() {
         <div>
           <div id="ways-contact">
             <div>
-              <p>
-                Want to bring more attention to your business?
-              </p>
-              
-              <h1 className="big fancy">
-                Let's grow together!
-              </h1>
+              <p>Want to bring more attention to your business?</p>
+
+              <h1 className="big fancy">Let's grow together!</h1>
             </div>
 
             <div>
               <div>
                 <div className="contact">
                   <figure>
-                    <img src={email} alt="Contact through E-mail" className="icons" />
+                    <img
+                      src={email}
+                      alt="Contact through E-mail"
+                      className="icons"
+                    />
                   </figure>
                 </div>
 
                 <p>
-                  <span>
-                    Send me an e-mail at:
-                  </span> <br />
-
+                  <span>Send me an e-mail at:</span> <br />
                   <strong>lucsas.lira@gmail.com</strong>
                 </p>
               </div>
@@ -55,70 +52,77 @@ function Contact() {
               <div>
                 <div className="contact">
                   <figure>
-                    <img src={instaLogo} alt="Contact through Instagram" className="icons" />
+                    <img
+                      src={instaLogo}
+                      alt="Contact through Instagram"
+                      className="icons"
+                    />
                   </figure>
                 </div>
 
                 <p>
-                  <span>
-                    Call me in Direct Message:
-                  </span> <br />
-
+                  <span>Call me in Direct Message:</span> <br />
                   <strong>@lucsas.l</strong>
                 </p>
               </div>
             </div>
-
           </div>
-          
+
           <div>
             <div className="center">
-              <h1 className="block-text" style={{ margin: "10px 0px 40px 0px" }}>
+              <h1
+                className="block-text"
+                style={{ margin: "10px 0px 40px 0px" }}
+              >
                 Contact
               </h1>
             </div>
-            
-            <form action="https://api.staticforms.xyz/submit" method="post" autoComplete="on">
+
+            <form
+              action="https://api.staticforms.xyz/submit"
+              method="post"
+              autoComplete="on"
+            >
               <div>
                 <div>
                   <label htmlFor="fullName">Name:</label> <br />
-                    <input 
-                      type="text"
-                      name="name"
-                      placeholder="Your name"
-                      value={nameInput}
-                      onChange={(e) => changeName(e)}
-                      required 
-                    />
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Your name"
+                    value={nameInput}
+                    onChange={(e) => changeName(e)}
+                    required
+                  />
                 </div>
 
                 <div>
                   <label htmlFor="emailAddress">E-mail: </label> <br />
-                    <input 
-                      type="email"
-                      name="email"
-                      placeholder="Your e-mail"
-                      value={emailInput}
-                      onChange={(e) => changeEmail(e)}
-                      required
-                    />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Your e-mail"
+                    value={emailInput}
+                    onChange={(e) => changeEmail(e)}
+                    required
+                  />
                 </div>
 
                 <div>
                   <label htmlFor="userMsg">Message: </label> <br />
-                    <textarea 
-                      name="message"
-                      cols="30"
-                      rows="10"
-                      placeholder="Write a message..."
-                      value={msgInput}
-                      onChange={(e) => changeMsg(e)}
-                      required>
-                    </textarea>
+                  <textarea
+                    name="message"
+                    cols="30"
+                    rows="10"
+                    placeholder="Write a message..."
+                    value={msgInput}
+                    onChange={(e) => changeMsg(e)}
+                    required
+                  ></textarea>
                 </div>
 
                 <div>
-                  <input 
+                  <input
                     type="submit"
                     value="Send"
                     className="submit-button"
@@ -126,20 +130,17 @@ function Contact() {
                 </div>
 
                 <div>
-                  <input 
+                  <input
                     type="hidden"
-                    name="accessKey" value="9f7e2bc4-d0c9-4819-ac06-2c58044142e0"
+                    name="accessKey"
+                    value="9f7e2bc4-d0c9-4819-ac06-2c58044142e0"
                   />
-                  <input 
+                  <input
                     type="hidden"
                     name="redirectTo"
                     value="document/success.html"
                   />
-                  <input 
-                    type="hidden"
-                    name="replyTo"
-                    value="@"
-                  />
+                  <input type="hidden" name="replyTo" value="@" />
                 </div>
               </div>
             </form>
@@ -147,7 +148,7 @@ function Contact() {
         </div>
       </section>
     </>
-  )
+  );
 }
 
 export default Contact;
