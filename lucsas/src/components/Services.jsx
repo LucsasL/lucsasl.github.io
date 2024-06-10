1ee1aed81bfe0592147dab4df8549b333b961edb
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-06-10 15:12:41 -0300

    Finally man

--- lucsas/src/components/Services.jsx
+++ lucsas/src/components/Services.jsx
@@ -1,5 +1,5 @@
 // Hooks Import
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 // Data Import
 import { webServiceSect } from "../utils/data";
@@ -9,28 +9,41 @@ function Services() {
   const [boxIntersect, setBoxIntersect] = useState(false);
   const infoBox = useRef([]);
   const servHeader = useRef(null);
- 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-        setBoxIntersect(true);
-      } else {
-        entry.target.classList.remove("show");
-        setBoxIntersect(false);
-      }
-    })
-  });
 
-  const intersection
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          infoBox.current.classList.add("show");
+          setBoxIntersect(entry.isIntersecting);
+        } else {
+          infoBox.current.classList.remove("show");
+          setBoxIntersect(entry.isIntersecting);
+        }
+      });
+    });
+
+    observer.observe(infoBox.current);
+  }, []);
+
+  const changeVisibility = () => {
+    console.log(boxIntersect);
+    return boxIntersect
+      ? {
+          opacity: 1,
+        }
+      : {
+          opacity: 0.3,
+        };
+  };
 
   return (
     <>
       <section id="services">
         <div>
           <div>
-            <h1 
-              className="block-text big" 
+            <h1
+              className="block-text big"
               style={{ margin: "30px 0px" }}
               ref={servHeader}
             >
@@ -39,37 +52,34 @@ function Services() {
           </div>
 
           <div id="cards">
-            {
-              servBoxes.map(({ title, text, img }, index) => {
-                return (
-                  <div 
-                    className="servBlock hidden" 
-                    key={index} 
-                    ref={infoBox}
-                  >
-                    <div className="imgSv">
-                      <figure>
-                        <img
-                          src={img}
-                          alt="Connection with your target audience"
-                          className="functions"
-                        />
-                      </figure>
-                    </div>
-
-                    <div className="imgSv"></div>
-
-                    <div>
-                      <h2 style={{ fontSize: "2em" }}>{title}</h2>
-
-                      <p>
-                        {text}
-                      </p>
-                    </div>
+            {servBoxes.map(({ title, text, img }, index) => {
+              return (
+                <div
+                  className="servBlock hidden"
+                  key={index}
+                  ref={infoBox}
+                  style={changeVisibility()}
+                >
+                  <div className="imgSv">
+                    <figure>
+                      <img
+                        src={img}
+                        alt="Connection with your target audience"
+                        className="functions"
+                      />
+                    </figure>
                   </div>
-                );
-              })
-            }
+
+                  <div className="imgSv"></div>
+
+                  <div>
+                    <h2 style={{ fontSize: "2em" }}>{title}</h2>
+
+                    <p>{text}</p>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </section>
