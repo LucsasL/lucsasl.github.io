3dfc89d6ab657e28c29246fb9ef468f19cf99519
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-06-12 09:51:40 -0300

    Update Skills.jsx

--- lucsas/src/sections/Skills.jsx
+++ lucsas/src/sections/Skills.jsx
@@ -1,54 +1,248 @@
 // Hooks Import
-import { useState, useReducer, useEffect, useRef } from "react";
+import { useState, useReducer, useEffect, useRef, act } from "react";
 
 // Images Import
 import notebook from "../img/google-ads-design.svg";
 
+// Font Import
+import plusJakartaSans from "../fonts/Plus_Jakarta_Sans/PlusJakartaSans-VariableFont_wght.ttf";
+
 // Data Import
 import { webSkillsSect } from "../utils/data";
 const { sectTitle, sectImg, techStack } = webSkillsSect;
-const { tech, techDesc } = techStack;
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "change_tech_HTML":
       return {
-        title: "HTML",
+        basicSettings: {
+          bg: "white",
+          color: "black",
+          font: "serif",
+        },
+
+        text: {
+          textSize: "1.2em",
+          textLine: "1em",
+        },
+
+        links: {
+          aDecoration: "underline",
+          aDisplay: "inline",
+          aBg: "none",
+          aColor: "blue",
+          aPadding: "0px",
+          aBorderRadius: "0px",
+        },
+
+        content: {
+          title: "HTML",
+          text: "The technology that gives meaning to websites through tags, building the document that you see in the browser.",
+          textLink: "",
+        },
       };
 
     case "change_tech_CSS":
       return {
-        title: "CSS",
+        basicSettings: {
+          bg: "#060606",
+          color: "white",
+          font: plusJakartaSans,
+        },
+
+        text: {
+          textSize: "1.5em",
+          textLine: "2em",
+        },
+
+        links: {
+          aDecoration: "none",
+          aDisplay: "inline",
+          aBg: "linear-gradient(to right, blue, #8000ff)",
+          aColor: "white",
+          aPadding: "12px 20px",
+          aBorderRadius: "10px",
+        },
+
+        content: {
+          title: "CSS",
+          text: "A combination with the stylesheets of HTML and CSS do the work for static websites, making possible to add layouts, positions, responsiveness and animations.",
+          textLink: "",
+        },
       };
 
     case "change_tech_JS":
       return {
-        title: "JavaScript",
+        basicSettings: {
+          bg: "#060606",
+          color: "white",
+          font: plusJakartaSans,
+        },
+
+        text: {
+          textSize: "1.5em",
+          textLine: "2em",
+        },
+
+        links: {
+          aDecoration: "none",
+          aDisplay: "inline",
+          aBg: "linear-gradient(to right, blue, #8000ff)",
+          aColor: "white",
+          aPadding: "12px 20px",
+          aBorderRadius: "10px",
+        },
+
+        content: {
+          title: "JavaScript",
+          text: "The technology that make things more interesting and add relevant features to a dynamic experience. With all of it's libraries, frameworks and DOM Manipulation bringing value, making the tech essential for web development.",
+          textLink: "",
+        },
       };
 
     case "change_tech_TS":
       return {
-        title: "TypeScript",
+        basicSettings: {
+          bg: "#060606",
+          color: "white",
+          font: plusJakartaSans,
+        },
+
+        text: {
+          textSize: "1.5em",
+          textLine: "2em",
+        },
+
+        links: {
+          aDecoration: "none",
+          aDisplay: "inline",
+          aBg: "linear-gradient(to right, blue, #8000ff)",
+          aColor: "white",
+          aPadding: "12px 20px",
+          aBorderRadius: "10px",
+        },
+
+        content: {
+          title: "TypeScript",
+          text: "Work In Progress",
+          textLink: "",
+        },
       };
 
     case "change_tech_React":
       return {
-        title: "React JS",
+        basicSettings: {
+          bg: "#060606",
+          color: "white",
+          font: plusJakartaSans,
+        },
+
+        text: {
+          textSize: "1.5em",
+          textLine: "2em",
+        },
+
+        links: {
+          aDecoration: "none",
+          aDisplay: "inline",
+          aBg: "linear-gradient(to right, blue, #8000ff)",
+          aColor: "white",
+          aPadding: "12px 20px",
+          aBorderRadius: "10px",
+        },
+
+        content: {
+          title: "React JS",
+          text: "Great Architecture and organization is what builds React JS, A technology that not only bring all the features of HTML, CSS and JavaScript combined, but enhance i as well, bringing state management, routers, and even more abstraction to your app.",
+          textLink: "",
+        },
       };
 
     case "change_tech_GSAP":
       return {
-        title: "GSAP",
+        basicSettings: {
+          bg: "#060606",
+          color: "white",
+          font: plusJakartaSans,
+        },
+
+        text: {
+          textSize: "1.5em",
+          textLine: "2em",
+        },
+
+        links: {
+          aDecoration: "none",
+          aDisplay: "inline",
+          aBg: "linear-gradient(to right, blue, #8000ff)",
+          aColor: "white",
+          aPadding: "12px 20px",
+          aBorderRadius: "10px",
+        },
+
+        content: {
+          title: "GSAP",
+          text: "Animation to your website ",
+          textLink: "",
+        },
       };
 
     case "change_tech_Redux":
       return {
-        title: "Redux",
+        basicSettings: {
+          bg: "#060606",
+          color: "white",
+          font: plusJakartaSans,
+        },
+
+        text: {
+          textSize: "1.5em",
+          textLine: "2em",
+        },
+
+        links: {
+          aDecoration: "none",
+          aDisplay: "inline",
+          aBg: "linear-gradient(to right, blue, #8000ff)",
+          aColor: "white",
+          aPadding: "12px 20px",
+          aBorderRadius: "10px",
+        },
+
+        content: {
+          title: "Redux",
+          text: "State Management that makes the structure more put together",
+          textLink: "",
+        },
       };
 
     case "change_tech_Git":
       return {
-        title: "Git",
+        basicSettings: {
+          bg: "#060606",
+          color: "white",
+          font: plusJakartaSans,
+        },
+
+        text: {
+          textSize: "1.5em",
+          textLine: "2em",
+        },
+
+        links: {
+          aDecoration: "none",
+          aDisplay: "inline",
+          aBg: "linear-gradient(to right, blue, #8000ff)",
+          aColor: "white",
+          aPadding: "12px 20px",
+          aBorderRadius: "10px",
+        },
+
+        content: {
+          title: "Git",
+          text: "An essential tool for project management and version control,",
+          textLink: "",
+        },
       };
 
     default:
@@ -58,18 +252,33 @@ const reducer = (state, action) => {
 
 function Skills() {
   const [infoIntersect, setInfoIntersect] = useState(false);
-  const [techListIndex, setTechListIndex] = useState(0);
-  const [activeTech, dispatch] = useReducer(
-    reducer,
-    {
+  const [activeTech, dispatch] = useReducer(reducer, {
+    basicSettings: {
       bg: "white",
       color: "black",
       font: "serif",
+    },
+
+    text: {
+      textSize: "1em",
+      textLine: "1em",
+    },
+
+    links: {
       aDecoration: "underline",
+      aDisplay: "inline",
+      aBg: "none",
+      aColor: "blue",
+      aPadding: "0px",
+      aBorderRadius: "0px",
+    },
+
+    content: {
       title: "HTML",
       text: "The technology that gives meaning to websites through tags, building the document that you see in the browser.",
-    }
-  );
+      textLink: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+    },
+  });
 
   const infoBox = useRef();
 
@@ -215,17 +424,39 @@ function Skills() {
                 </figure>
               </picture>
 
-              <div className="laptopContent" style={{}}>
-                <h1>{`This is ${activeTech.title}`}</h1>
+              <div
+                className="laptopContent"
+                style={{
+                  background: activeTech.basicSettings.bg,
+                  color: activeTech.basicSettings.color,
+                  fontFamily: activeTech.basicSettings.font,
+                }}
+              >
+                <h1>{`This is ${activeTech.content.title}`}</h1>
 
-                <p>{techDesc[techListIndex]}</p>
+                <p
+                  style={{
+                    fontSize: activeTech.text.textSize,
+                    lineHeight: activeTech.text.textLine,
+                  }}
+                >
+                  {activeTech.content.text}
+                </p>
 
                 <a
-                  href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+                  href={activeTech.content.textLink}
                   target="_blank"
                   rel="noopener noreferrer"
+                  style={{
+                    background: activeTech.links.aBg,
+                    color: activeTech.links.aColor,
+                    padding: activeTech.links.aPadding,
+                    borderRadius: activeTech.links.aBorderRadius,
+                    textDecoration: activeTech.links.aDecoration,
+                    display: activeTech.links.aDisplay,
+                  }}
                 >
-                  Learn more about {tech[techListIndex]}
+                  Learn more about {activeTech.content.title}
                 </a>
               </div>
             </div>
