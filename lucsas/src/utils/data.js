09e9cc22e6d0acfee8b2e414cee97e1b75bd1765
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-06-15 11:44:40 -0300

    Data completely translated for Portuguese

--- lucsas/src/utils/data.js
+++ lucsas/src/utils/data.js
@@ -47,6 +47,14 @@ export const webNavOptions = ["Home", "About", "Skills", "Projects", "Contact"];
 export const webNavOptionsLink = ["intro", "services", "skills", "portfolio", "contact"];
 
 export const webIntroSect = {
+  info: {
+    h1Span1: "Sup, I'm",
+    h1Span2: "Lucsas",
+    h1Span3: "Lucas Lira",
+    text: "I create and optimize websites with React and JavaScript,delivering the best to make good software that solves real problems.",
+    downloadButt: "Download Resume",
+  },
+  introImg: code,
   socialMedias: [
     {
       link: "https://www.instagram.com/lucsas.l/",
@@ -69,7 +77,6 @@ export const webIntroSect = {
       img: twitterLogo,
     },
   ],
-  introImg: code,
 };
 
 export const webServiceSect = {
@@ -77,12 +84,12 @@ export const webServiceSect = {
   servBoxes: [
     {
       title: "Semantics and SEO",
-      text: "Structuring a website semantically, respecting the basics of SEO and giving meaning to the components and objects of a website.",
+      text: "Structuring a website semantically, respecting the basic concepts of SEO and giving meaning to the components and objects of a website.",
       img: seo,
     },
     {
       title: "Responsive Design",
-      text: "Giving shapes and layouts that work in every way, adapting to the sizes and devices that show presence for you everywhere.",
+      text: "Giving shapes and layouts that work in every way and applying medias that work respecting performance, making your site adapt to all sizes and devices, showing your presence everywhere.",
       img: design,
     },
     {
@@ -96,8 +103,8 @@ export const webServiceSect = {
 export const webAboutSect = {
   sectTitle: "Hi, My name is Lucas Lira",
   aboutText: [
-    "I work as a <strong>Front-End Web Developer;</strong> working with interfaces and consuming APIs for websites. My expertise lies in crafting dynamic, visually appealing, and user-friendly web applications. Having gained substantial experience in my abilities, I am now looking to work as a Junior Developer.",
-    "Since I began, I've been studying topics in the field of programming for a while now, always looking to improve and get better at delivering what is necessary to enhance my applications. My GitHub shows how much effort I put into really understanding the technologies.",
+    "I work as a <strong>Front-End Web Developer;</strong> working with interfaces and consuming APIs for websites. My expertise lies in crafting dynamic, visually appealing, and user-friendly web applications. Having gained substantial experience in my abilities, I am now looking to work as a Developer.",
+    "Since I began, I've been studying topics in the field of programming for a while now, always looking to improve and deliver what is necessary to enhance my applications. My GitHub shows how much effort I put into really understanding the technologies.",
     "My job and commitment goes to give results, whether this being coding, projecting, or giving ideas.",
     "What do you thing? Can we grow together?",
   ],
@@ -126,7 +133,7 @@ export const webSkillsSect = {
     imgs: [html, css, js, ts, react, sass, gsap, jest, redux, python, git],
     techDesc: [
       "The technology that gives meaning to websites through tags, building the document that you see in the browser.",
-      "A combination with the stylesheets of HTML and CSS do the work for static websites, making possible to add layouts, positions, responsiveness and animations.",
+      "The combination between the HTML tags and stylesheets of CSS do the work for static websites, making possible to add layouts, positions, responsiveness and animations.",
       "The technology that make things more interesting and add relevant features to a dynamic experience. With all of it's libraries, frameworks and DOM Manipulation bringing value, making the tech essential for web development.",
       "Great Architecture and organization is what builds React JS, A technology that not only bring all the features of HTML, CSS and JavaScript combined, but enhance it as well, bringing state management, routers, and even more abstraction to your app.",
       "Animation to your website ",
@@ -138,14 +145,14 @@ export const webSkillsSect = {
 };
 
 export const webProjectsSect = {
-  sectTitle: "In what did you work?",
+  sectTitle: "In what projects did you work?",
   projects: [
     {
       projLink: "https://marknow-lemon.vercel.app/",
       projTitle: "MARKNOW",
       projSubtitle: "A Digital Marketing Agency that brings the success",
       projDesc:
-        "MARKNOW is a web project showcasing a digital marketing company that has great numbers and partners. Fully build with React JS and it's technologies. It has several features, such as Hooks, States and Routers. | WORK IN PROGRESS",
+        "MARKNOW is a web project showcasing a digital marketing company that has great feats and partners. Fully build with React JS and it's technologies. It has several features, such as Hooks, States and Routers. | WORK IN PROGRESS",
       projImgDesc: "Markdown project image",
       techStack: [
         "HTML",
@@ -209,63 +216,4 @@ export const webProjectsSect = {
 
 export const webContactSect = {
   sectTitle: "Want to make your business software real? Let's grow together!",
-};
-
-export const ppcNavOptions = [
-  "Home",
-  "About",
-  "Google Ads",
-  "Meta Ads",
-  "Contact",
-];
-
-export const ppcIntroSect = {};
-
-export const ppcServiceSect = {
-  sectTitle: "In what can I help you?",
-  campaignBox: {
-    title: "Campaign Creation",
-    text: "Create optimized campaigns to potential clients in different segmentation and locations. Making your business reach new clients in a better way and promoting your branch.",
-  },
-  copywriteBox: {
-    title: "Captivating Writing",
-    text: "With good use of Copywriting, we can convince more people and attract impressions, leads and conversions.",
-  },
-  multiPlatformBox: {
-    title: "Multi-Platform",
-    text: "Expanding your brand in different social medias and all different places of internet, bringing more sight for you.",
-  },
-};
-
-export const ppcAboutSect = {
-  sectTitle: "Hi, My name is Lucas Lira",
-  par1: "I work as a Pay-Per-Click Manager, a professional which main function is to create, manage and optimizing ads campaign in different online advertising softwares, but the most important function, is to bring results for my clients.",
-  par2: "I've been studying topics surrounding the fields of Digital Marketing and Programming, always looking to improve and get better in delivering what you need.",
-  par3: "My job and commitment goes to give results, whether this being conversions, engagement, leads or even more recognition to your work.",
-  par4: "What do you thing? Can we grow together?",
-};
-
-export const ppcPlatformSect = {
-  googleAds: {
-    title: "Google Ads Specialist",
-    text: "Meticulously target your audience through the lens of Google Ads, ensuring that every click counts. Your business spotted by the right customer by the right advert. Using Audience Segmentation we can track all the different types of interests and ideas, and use these statistics to bring more potential clients to you.",
-  },
-  metaAds: {
-    title: "Reach beyond the bounds with Meta",
-    text: "Elevate your social media presence and connect with your audience on a deeper level through Meta Ads. From Facebook to Instagram, we can implement targeting strategies that resonate with your audience, driving conversions in the social spaces where your customers live, share, and interact.",
-  },
-};
-
-export const ppcFunctionsSect = {
-  sectTitle: "What does a PPC Manager do?",
-  par1: "Make you reach any type of person on the internet through ads campaign.",
-  par2: "Make your clients pay attention to your products or services on the biggest attention attractor, the internet.",
-  par3: "Often reports of the results of your campaigns and the strategies used to make it.",
-  par4: "Use strategies and segmentation to appear to the persons that matter the most to you.",
-  par5: "Sell the right product, to the right person in the right time, focusing only in the target audience of yours.  ",
-};
-
-export const ppcContactSect = {
-  sectTitle:
-    "Want to bring more attention to your business? Let's grow together!",
 };
---
+++ lucsas/src/utils/dataPortuguese.js
@@ -0,0 +1,225 @@
+// Image Import
+import code from "../img/codeIntro.png";
+import lucsas from "../img/Lucsas.webp";
+import webDev from "../img/webDev.webp";
+
+import marknow from "../img/marknow-proj.png";
+import everyshop from "../img/everyshop-proj.png";
+
+import seo from "../img/setting.png";
+import design from "../img/web-developer.png";
+import webStructure from "../img/web-structure.png";
+
+// Tech Stack Images Import
+import html from "../img/html5.webp";
+import css from "../img/css3.webp";
+import ts from "../img/typescript.webp";
+import js from "../img/JS.webp";
+import react from "../img/react.webp";
+import sass from "../img/sass.webp";
+import python from "../img/python.webp";
+import jest from "../img/jest.webp";
+import gsap from "../img/gsap.webp";
+import redux from "../img/redux.webp";
+import git from "../img/git.webp";
+
+import htmlBig from "../img/html5-big.webp";
+import cssBig from "../img/css-big.webp";
+import jsBig from "../img/JS-big.webp";
+import tsBig from "../img/typescript-big.webp";
+import reactBig from "../img/react-big.webp";
+import sassBig from "../img/sass-big.webp";
+import jestBig from "../img/jest-big.webp";
+import reduxBig from "../img/redux-big.webp";
+import gsapBig from "../img/gsap-big.webp";
+import gitBig from "../img/git-big.webp";
+
+// Social Media Image Import
+import instaLogo from "../img/instagram-social.webp";
+import githubLogo from "../img/github-social.webp";
+import linkedinLogo from "../img/linkedin-social.webp";
+import twitterLogo from "../img/twitter-social.webp";
+
+// Components Data
+export const contentWebDevPortfolio = {};
+
+export const webNavOptions = ["Home", "Sobre", "Habilidades", "Projetos", "Contato"];
+export const webNavOptionsLink = [
+  "intro",
+  "services",
+  "skills",
+  "portfolio",
+  "contact",
+];
+
+export const webIntroSect = {
+  info: {
+    h1Span1: "Salve, eu sou",
+    h1Span2: "Lucsas",
+    h1Span3: "Lucas Lira",
+    text: "Eu crio e otimizo websites with React e JavaScript, entregando o melhor para fazer software bons que resolvem problemas reais.",
+    downloadButt: "Baixar Currículo",
+  },
+  introImg: code,
+  socialMedias: [
+    {
+      link: "https://www.instagram.com/lucsas.l/",
+      name: "Lucsas Instagram",
+      img: instaLogo,
+    },
+    {
+      link: "http://github.com/lucsasl",
+      name: "Lucsas Github",
+      img: githubLogo,
+    },
+    {
+      link: "https://www.linkedin.com/in/lucsas/?locale=en_US",
+      name: "Lucsas Linkedin",
+      img: linkedinLogo,
+    },
+    {
+      link: "https://twitter.com/LucsasL",
+      name: "Lucsas Twitter",
+      img: twitterLogo,
+    },
+  ],
+};
+
+export const webServiceSect = {
+  sectTitle: "Em que posso ajudar?",
+  servBoxes: [
+    {
+      title: "Semântica e SEO",
+      text: "Estruturar um website semanticamente, respeitando os conceitos básicos de SEO e dando significado aos componentes e objetos de um site.",
+      img: seo,
+    },
+    {
+      title: "Design Responsivo",
+      text: "Dando formas e layouts que funcionam de qualquer maneira e aplicando mídias que funcionam respeitando performance, de modo em que seu site se adapte aos tamanhos e dispositivos automaticamente, mostrando sua presença em qualquer lugar de forma clara.",
+      img: design,
+    },
+    {
+      title: "Aplicações Dinâmicas",
+      text: "Desenvolvendo websites com uma imensa variedade de tecnologias, variando de tecnologias que apenas constroem componentes simples até outras que fazem estruturas complexas e arquiteturas robustas.",
+      img: webStructure,
+    },
+  ],
+};
+
+export const webAboutSect = {
+  sectTitle: "Eae, Meu nome é Lucas Lira",
+  aboutText: [
+    "Eu trabalho como um <strong>Desenvolvedor Web Front-End;</strong> trabalhando com interfaces e consumindo APIs para websites. Minha expertise se baseia em criar aplicações web dinâmicos, versáteis e visualmente agradáveis. Tendo ganhado experiência substancial com minhas habilidades, estou buscando trabalhar como um Desenvolvedor.",
+    "Desde que comecei, Estive estudando tópicos e tecnologias da computação por um tempo e registrando esse meu avanço, sempre buscando melhorar e entregar o que é necessário para desenvolver uma aplicação aprimorada. Meu GitHub mostra o caminho que trilhei para entender as tecnologias que rondam esse mercado.",
+    "Meu trabalho e compromisso vai para dar resultados, independente da forma, sendo isso escrevendo código, projetando ou dando ideias.",
+    "O que você acha? Podemos crescer juntos?",
+  ],
+  aboutImg: lucsas,
+};
+
+export const webSkillsSect = {
+  sectTitle: "Conjunto de Tecnologias que eu já usei:",
+  sectImg: webDev,
+  areaDesc:
+    "Eu estive pondo dedicação em desenvolver minhas habilidades em HTML, CSS, JavaScript, React JS, GSAP, Redux, e Git.",
+  techStack: {
+    tech: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "TypeScript",
+      "React JS",
+      "SASS",
+      "GSAP",
+      "Jest",
+      "Redux",
+      "Python",
+      "Git",
+    ],
+    imgs: [html, css, js, ts, react, sass, gsap, jest, redux, python, git],
+    techDesc: [
+      "A tecnologia que dá significado aos websites através de suas tags, construindo o documento que você vê no navegador.",
+      "A combinação entre as tags do HTML e as folhas de estilo CSS fazem o trabalho para sites estáticos, tornando possível a adição de layouts, posições, responsividade e animações.",
+      "A tecnologia que faz as coisas mais interessantes e adiciona atributos relevantes para uma experiência dinâmica. Com toda suas bibliotecas, frameworks e manipulação de DOM dando valor à aplicação, tornando o JavaScript indispensável no desenvolvimento Web.",
+      "Ótima arquitetura e organização é o que faz o React JS, uma tecnologia que não apenas traz todos os atributos do HTML, CSS e JavaScript combinados, mas as melhora também, trazendo gerenciamento estados, roteadores, e até mais abstrações para sua aplicação.",
+      "Animações para seu website",
+      "Pré-processador de CSS",
+      "Gerenciamento de estados que fazem as estruturas mais organizadas",
+      "Uma ferramenta essencial para gerenciamento de projetos e controle de versão,",
+    ],
+  },
+};
+
+export const webProjectsSect = {
+  sectTitle: "Em que projetos você já trabalhou?",
+  projects: [
+    {
+      projLink: "https://marknow-lemon.vercel.app/",
+      projTitle: "MARKNOW",
+      projSubtitle: "Uma Agência de Marketing que traz sucesso",
+      projDesc:
+        "MARKNOW é um projeto web apresentando uma empresa de marketing digital que tem grandes feitos e parceiros. Cosntruído com base em React JS e suas tecnologias. Tendo inúmeras características, como Hooks, Estados e Roteadores. | TRABALHO EM PROGRESSO",
+      projImgDesc: "Imagem do projeto MARKNOW",
+      techStack: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "TypeScript",
+        "React JS",
+        "SASS",
+        "GSAP",
+        "Jest",
+        "Git",
+      ],
+      techStackImg: [
+        htmlBig,
+        cssBig,
+        jsBig,
+        tsBig,
+        reactBig,
+        sassBig,
+        gsapBig,
+        jestBig,
+        gitBig,
+      ],
+      projImg: marknow,
+    },
+    {
+      projLink: "https://every-shop.vercel.app/",
+      projTitle: "Everyshop",
+      projSubtitle: "Uma loja que vende de tudo",
+      projDesc:
+        "EVERYSHOP é um projeto web de uma loja que vende de tudo. Desenvolvido em React JS incluindo SASS e design responsivo. | TRABALHO EM PROGRESSO",
+      projImgDesc: "Imagem do projeto EVERYSHOP",
+      techStack: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "TypeScript",
+        "React JS",
+        "SASS",
+        "GSAP",
+        "Redux",
+        "Jest",
+        "Git",
+      ],
+      techStackImg: [
+        htmlBig,
+        cssBig,
+        jsBig,
+        tsBig,
+        reactBig,
+        sassBig,
+        gsapBig,
+        reduxBig,
+        jestBig,
+        gitBig,
+      ],
+      projImg: everyshop,
+    },
+  ],
+};
+
+export const webContactSect = {
+  sectTitle: "Quer tornar o software da sua empresa real? Vamos crescer juntos!",
+};
