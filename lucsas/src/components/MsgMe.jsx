bd7e6433ec52102302d6e110b5d224f8cd445796
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-06-28 20:34:15 -0300

    Adding button to go to the top of the page

--- lucsas/src/components/MsgMe.jsx
+++ lucsas/src/components/MsgMe.jsx
@@ -4,13 +4,31 @@ import { useContext, useState } from "react";
 // Icons
 import { CgMenuRightAlt } from "react-icons/cg";
 import { IoMdClose } from "react-icons/io";
+import { FaArrowUp } from "react-icons/fa";
 
 // Context
 import { PageLang } from "../App";
 
 function MsgMe() {
+  // States
   const [menuShown, setMenuShown] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
+  window.addEventListener("scroll", (e) => {
+    const posY = window.scrollY;
+
+    if (posY > 0) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  });
+
+  const pageToTop = () => {
+    window.scrollTo(0, 0);
+  }
+
+  // Context
   const { setLang } = useContext(PageLang);
 
   return (
@@ -32,17 +50,45 @@ function MsgMe() {
           pointerEvents: menuShown ? "" : "none",
         }}
       >
-        <abbr title={"Translate to Portugues"}>
-          <select name="Translate" id="changeLang">
-            <option value="English" onSelect={() => setLang("English")}>
-              English
-            </option>
-            <option value="Portuguese" onSelect={() => setLang("Portuguese")}>
-              Portuguese
-            </option>
-          </select>
-        </abbr>
+        <div>
+          <abbr title={"Translate to Portugues"}>
+            <select name="Translate" id="changeLang">
+              <option value="English" onSelect={() => setLang("English")}>
+                English
+              </option>
+              <option value="Portuguese" onSelect={() => setLang("Portuguese")}>
+                Portuguese
+              </option>
+            </select>
+          </abbr>
+        </div>
+
+        <div className="socials">
+          <abbr title="MSG ME">
+            <button>
+              <a
+                href="https://www.linkedin.com/in/lucsas"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                MSG ME
+              </a>
+            </button>
+          </abbr>
+        </div>
       </div>
+
+      <button
+        className="backUp"
+        style={{
+          opacity: scrolled ? "1" : "0",
+          pointerEvents: scrolled ? "" : "none",
+          left: menuShown ? "5%" : "62%",
+        }}
+        onClick={() => pageToTop()}
+      >
+        <FaArrowUp />
+      </button>
     </>
   );
 }
